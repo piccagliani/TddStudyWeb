c563e016f0b858b4cbecda28e097eac9f37bf310
piccagliani <piccagliani.dev@gmail.com>
2013-08-12 16:11:50 +0900

    Add Twitter Bootstrap 3 & Change Routing

--- src/DevMStudy/TaskManageBundle/Controller/TaskManageController.php
+++ src/DevMStudy/TaskManageBundle/Controller/TaskManageController.php
@@ -9,19 +9,21 @@ class TaskManageController extends Controller
     public function listAction()
     {
         return $this->render('DevMStudyTaskManageBundle:TaskManage:list.html.php');
-
     }
 
     public function createAction()
     {
+        return $this->render('DevMStudyTaskManageBundle:TaskManage:create.html.php');
     }
 
     public function updateAction($id)
     {
+        return $this->render('DevMStudyTaskManageBundle:TaskManage:update.html.php');
     }
 
     public function deleteAction($id)
     {
+        return $this->render('DevMStudyTaskManageBundle:TaskManage:delete.html.php');
     }
 
 }
--- src/DevMStudy/TaskManageBundle/Resources/views/layout.html.php
+++ src/DevMStudy/TaskManageBundle/Resources/views/layout.html.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><?php $view['slots']->output('title', 'Hello Application') ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<div class="container">
 <?php $view['slots']->output('body') ?>
+</div>
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
 </body>
 </html>
