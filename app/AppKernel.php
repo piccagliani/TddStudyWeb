528a01e1fb54e67a2b61dc8498b6284688fcf38c
piccagliani <piccagliani.dev@gmail.com>
2013-10-08 14:25:13 +0900

    composer update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new DevMStudy\TaskManageBundle\DevMStudyTaskManageBundle(),
         );
 
