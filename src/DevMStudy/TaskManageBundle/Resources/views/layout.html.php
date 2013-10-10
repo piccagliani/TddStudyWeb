11514b149d286938e22d97e3d9034e1269593f33
piccagliani <piccagliani.dev@gmail.com>
2013-10-10 14:54:34 +0900

    Update bootstrap 3.0.0-rc -> 3.0.0

--- src/DevMStudy/TaskManageBundle/Resources/views/layout.html.php
+++ src/DevMStudy/TaskManageBundle/Resources/views/layout.html.php
@@ -4,13 +4,13 @@
     <title><?php $view['slots']->output('title', 'Hello Application') ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
 <?php $view['slots']->output('body') ?>
 </div>
 <script src="//code.jquery.com/jquery.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
